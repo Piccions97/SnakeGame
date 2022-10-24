22a8ce29157a66cdc17bc18e0676404c50d562c4
Antonio Piccione <toni.piccione@gmail.com>
2022-10-24 17:33:19 +0200

    feat: writed structures of classes Snake, Food. These classes representing the Snake object and Food object of the game. Basic use of OOP. No moving already implemented.

--- main.py
+++ main.py
@@ -5,22 +5,36 @@ from turtle import window_height
 GAME_WIDTH= 700
 GAME_HEIGHT= 700
 SPEED = 50
-SPACE = 50
+SPACE_SIZE = 50
 BODY_PARTS = 3
 SNAKE_COLOR= "#00FF00"
 FOOD_COLOR="#FF0000"
 BACKGROUND_COLOR="#000000"
 
 class Snake:
-    pass
+    def __init__(self):
+        self.body_size=BODY_PARTS
+        self.coordinates=[]
+        self.squares=[]
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append([0, 0])
+        
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
+            self.squares.append(square)
 
 class Food:
-    pass
+    def __init__(self):
+        x = random.randint(0, (GAME_WIDTH/SPACE_SIZE)-1)*SPACE_SIZE
+        y = random.randint(0, (GAME_HEIGHT/SPACE_SIZE)-1)*SPACE_SIZE
+
+        self.coordinates = [x, y]
+        canvas.create_oval(x, y, x+SPACE_SIZE, y+SPACE_SIZE, fill= FOOD_COLOR, tag="food")
 
 def next_turn():
-    pass 
+    pass
 
-def change_diretion(new_direction):
+def change_direction(new_direction):
     pass
 
 def check_collisions():
@@ -29,7 +43,7 @@ def check_collisions():
 def game_over():
     pass
 
-window= Tk();
+window = Tk()
 window.title("Snake Game")
 window.resizable(False, False)
 
