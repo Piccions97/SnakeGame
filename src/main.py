48a9708a170ddf5673ba1ef18f43dfefdfccc2f0
Antonio Piccione <toni.piccione@gmail.com>
2022-11-09 23:17:20 +0100

    Try to fix no gui on remote github terminal

--- src/main.py
+++ src/main.py
@@ -3,6 +3,11 @@ import random
 from turtle import window_height
 import tkinter as tk
 from numpy import square
+import os
+
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using :0.0')
+    os.environ.__setitem__('DISPLAY', ':0.0')
 
 GAME_WIDTH= 1000
 GAME_HEIGHT= 700
