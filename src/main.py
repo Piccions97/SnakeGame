157b479e6b88927deb0b8490a497da8841d96585
Antonio Piccione <toni.piccione@gmail.com>
2022-11-09 23:37:06 +0100

    third test for fix no gui on github

--- src/main.py
+++ src/main.py
@@ -3,11 +3,10 @@ import random
 from turtle import window_height
 import tkinter as tk
 from numpy import square
-import os
 
-if os.environ.get('DISPLAY','') == '':
-    print('no display found. Using :0.0')
-    os.environ.__setitem__('DISPLAY', ':0.0')
+import os
+os.system('Xvfb :1 -screen 0 1600x1200x16  &')    # create virtual display with size 1600x1200 and 16 bit color. Color can be changed to 24 or 8
+os.environ['DISPLAY']=':1.0'    # tell X clients to use our virtual DISPLAY :1.0.
 
 GAME_WIDTH= 1000
 GAME_HEIGHT= 700
